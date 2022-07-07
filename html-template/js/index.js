b242e17caa60f3d8661e0813dbf6f5dee93ae626
BenSegni <BBKSegni@hotmail.com>
2022-07-07 12:41:22 +0100

    code clean up

--- html-template/js/index.js
+++ html-template/js/index.js
@@ -1,22 +1,232 @@
-/**
- * declare global variables
- */
+//@ts-check
+/** @type { { title: string, subTitle: string, link: string, linkText: string } } */
 let hero;
+/** @type {{ heroImage: string, profileImages: [ { image: string, title: string, text: string, date: string, link: string, linkText: string } ] }} */
 let images;
-let nav;
-let heroSection;
-let profileSection;
-let footer;
+/** @type { { socialLinks: [ { type: string, link: string } ] } } */
 let social;
+/** @type {HTMLElement} */
+let nav;
+/** @type {HTMLElement} */
+let heroSection;
+/** @type {HTMLElement} */
+let profileSection;
+/** @type {HTMLElement} */
+let footer;
 /**
- * get nav data and assign social media buttons
+ * create Logo Class
  */
-assignSocialMedia = () => {
-  //create a logo for the site
-  const logo = document.createElement("img");
-  logo.classList.add("logo");
-  logo.src = `./img/logo${".png" ? ".png" : ".jpg"}`;
-  nav.appendChild(logo);
+class Logo {
+  constructor() {
+    this.logo = document.createElement("img");
+    this.logo.classList.add("logo");
+    this.logo.src = `./img/logo${".png" ? ".png" : ".jpg"}`;
+    this.logo.onclick = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    };
+    nav.appendChild(this.logo);
+  }
+}
+/**
+ * create Social Media Icons Class
+ */
+class socialMediaIcons {
+  constructor(iconObject) {
+    this.createLink(iconObject);
+    this.createIcon(iconObject);
+  }
+
+  createLink = (iconObject) => {
+    this.link = document.createElement("a");
+    this.link.href = iconObject.link;
+    this.link.target = "_blank";
+    this.link.classList.add("icon");
+    nav.appendChild(this.link);
+  };
+
+  createIcon = (iconObject) => {
+    this.icon = document.createElement("img");
+    this.icon.src = `./img/${iconObject.type}.svg`;
+    this.link.appendChild(this.icon);
+  };
+}
+/**
+ * create Hero Class
+ */
+class Hero {
+  constructor(hero) {
+    this.createWrapper();
+    this.createTitle(hero);
+    this.createSubText(hero);
+    this.createHeroButton(hero);
+    heroSection.appendChild(this.heroWrapper);
+  }
+
+  createWrapper = () => {
+    this.heroWrapper = document.createElement("div");
+    this.heroWrapper.classList.add("hero-wrapper");
+  };
+
+  createTitle = (hero) => {
+    this.header = document.createElement("h1");
+    this.heroTitleText = document.createTextNode(
+      `${hero.title ? hero.title : ""}`
+    );
+    this.header.appendChild(this.heroTitleText);
+    this.heroWrapper.appendChild(this.header);
+  };
+
+  createSubText = (hero) => {
+    this.subTitle = document.createElement("p");
+    this.subTitleText = document.createTextNode(
+      `${hero.subTitle ? hero.subTitle : ""}`
+    );
+    this.subTitle.appendChild(this.subTitleText);
+    this.heroWrapper.appendChild(this.subTitle);
+  };
+
+  createHeroButton = (hero) => {
+    if (hero.link && hero.linkText) {
+      this.button = document.createElement("a");
+      this.button.classList.add("hero-button");
+      this.buttonText = document.createTextNode(hero.linkText);
+      this.button.appendChild(this.buttonText);
+      this.button.href = hero.link;
+      this.button.target = "_blank";
+      this.heroWrapper.appendChild(this.button);
+    }
+  };
+}
+/**
+ * create ImageProfile class
+ */
+class ImageProfile {
+  constructor(image) {
+    this.createImageWrapper();
+    this.createTextWrapper(image);
+    this.createDate(image);
+    this.createDescription(image);
+    this.createButton(image);
+    if (this.textWrapper) {
+      this.imageWrapper.appendChild(this.textWrapper);
+      profileSection.appendChild(this.imageWrapper);
+    }
+  }
+
+  createImageWrapper = () => {
+    this.imageWrapper = document.createElement("div");
+    this.imageWrapper.classList.add("profile-image");
+  };
+
+  createTextWrapper = (image) => {
+    this.textWrapper = document.createElement("div");
+    this.title = document.createElement("h2");
+    this.titleText = document.createTextNode(`${image.title}`);
+    this.title.appendChild(this.titleText);
+  };
+
+  createDate = (image) => {
+    this.date = document.createElement("span");
+    if (image.date) {
+      this.date.classList.add("date");
+      this.dateValue = document.createTextNode(
+        new Date(`${image.date}`).toDateString()
+      );
+      this.date.appendChild(this.dateValue);
+    }
+  };
+
+  createDescription = (image) => {
+    this.textWrapper.classList.add("description");
+    this.description = document.createTextNode(`${image.text}`);
+    this.textWrapper.appendChild(this.title);
+    if (image.date) {
+      this.textWrapper.appendChild(this.date);
+    }
+    this.textWrapper.appendChild(this.description);
+  };
+
+  createButton = (image) => {
+    if (image.link && image.linkText) {
+      const link = document.createElement("a");
+      const linkText = document.createTextNode(`${image.linkText}`);
+      link.appendChild(linkText);
+      link.classList.add("link");
+      link.href = image.link;
+      link.target = "_blank";
+      this.textWrapper.appendChild(link);
+    }
+    this.imageWrapper.style.backgroundImage = `url(${
+      image.image ? image.image : "none"
+    })`;
+  };
+}
+/**
+ * create a Footer Class with social media data
+ */
+class FooterIcon {
+  constructor(listElement, socialObject) {
+    this.createFooterIcon(socialObject);
+    this.createFooterLink(socialObject);
+    this.createFooterList(listElement);
+  }
+
+  createFooterIcon = (socialObject) => {
+    this.footerIcon = document.createElement("img");
+    this.footerIcon.src = `./img/${socialObject.type}.svg`;
+  };
+
+  createFooterLink = (socialObject) => {
+    this.footerLink = document.createElement("a");
+    this.footerLink.href = socialObject.link;
+    this.linkText = document.createTextNode(`${socialObject.type}`);
+    this.footerLink.appendChild(this.linkText);
+  };
+
+  createFooterList = (listElement) => {
+    this.listItem = document.createElement("li");
+    this.listItem.appendChild(this.footerIcon);
+    this.listItem.appendChild(this.footerLink);
+    listElement.appendChild(this.listItem);
+    footer.appendChild(listElement);
+  };
+}
+
+class FooterNavigationArrow {
+  constructor() {
+    this.createArrowIcon();
+    footer.appendChild(this.arrow);
+  }
+
+  createArrowIcon = () => {
+    this.arrow = document.createElement("img");
+    this.arrow.src = "./img/up-arrow.svg";
+    this.arrow.classList.add("up-arrow");
+    this.arrow.onclick = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    };
+  };
+}
+/**
+ *
+ * @returns a new Logo Object
+ */
+const createLogo = () => {
+  return new Logo();
+};
+/**
+ * get social icon data and return new icons
+ */
+const assignSocialMediaIcons = () => {
+  createLogo();
   fetch("./json/social.json")
     .then((social) => {
       return social.json();
@@ -24,75 +234,24 @@ assignSocialMedia = () => {
     .then((data) => {
       social = data;
       social?.socialLinks.map((link) => {
-        createSocialMediaButtons(link);
+        return new socialMediaIcons(link);
       });
     });
 };
 /**
- * create social media buttons
- */
-createSocialMediaButtons = (button) => {
-  const link = document.createElement("a");
-  link.href = button.link;
-  link.target = "_blank";
-  link.classList.add("icon");
-  const icon = document.createElement("img");
-  icon.src = `./img/${button.type}.svg`;
-  link.appendChild(icon);
-  nav.appendChild(link);
-  //add click event to logo
-  const logo = document.querySelector(".logo");
-  logo.onclick = () => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-  };
-};
-/**
  * get hero data and create elements
  */
-assignHero = () => {
+const assignHero = () => {
   fetch("./json/hero.json")
     .then((hero) => {
       return hero.json();
     })
     .then((data) => {
-      hero = data;
-      const heroWrapper = document.createElement("div");
-      heroWrapper.classList.add("hero-wrapper");
-      //create header text
-      const header = document.createElement("h1");
-      const heroTitleText = document.createTextNode(
-        `${hero.title ? hero.title : ""}`
-      );
-      //create subtitle text
-      const subTitle = document.createElement("p");
-      const subTitleText = document.createTextNode(
-        `${hero.subTitle ? hero.subTitle : ""}`
-      );
-      subTitle.appendChild(subTitleText);
-      header.appendChild(heroTitleText);
-      heroWrapper.appendChild(header);
-      heroWrapper.appendChild(subTitle);
-      //create link button
-      if (hero.link && hero.linkText) {
-        const button = document.createElement("a");
-        button.classList.add("hero-button");
-        const buttonText = document.createTextNode(hero.linkText);
-        button.appendChild(buttonText);
-        button.href = hero.link;
-        button.target = "_blank";
-        heroWrapper.appendChild(button);
-      }
-      heroSection.appendChild(heroWrapper);
+      return new Hero(data);
     });
 };
-/**
- * get image data and create elements
- */
-assignImages = () => {
+
+const assignImages = () => {
   fetch("./json/images.json")
     .then((images) => {
       return images.json();
@@ -103,51 +262,14 @@ assignImages = () => {
         images.heroImage ? images.heroImage : "none"
       })`;
       images?.profileImages?.map((image) => {
-        //create image wrappers
-        const imageWrapper = document.createElement("div");
-        imageWrapper.classList.add("profile-image");
-        //create text wrappers with text & links
-        const textWrapper = document.createElement("div");
-        const title = document.createElement("h2");
-        const titleText = document.createTextNode(`${image.title}`);
-        title.appendChild(titleText);
-        const date = document.createElement("span");
-        if (image.date) {
-          date.classList.add("date");
-          const dateValue = document.createTextNode(
-            new Date(`${image.date}`).toDateString()
-          );
-          date.appendChild(dateValue);
-        }
-        textWrapper.classList.add("description");
-        const description = document.createTextNode(`${image.text}`);
-        textWrapper.appendChild(title);
-        if (image.date) {
-          textWrapper.appendChild(date);
-        }
-        textWrapper.appendChild(description);
-        if (image.link && image.linkText) {
-          const link = document.createElement("a");
-          const linkText = document.createTextNode(`${image.linkText}`);
-          link.appendChild(linkText);
-          link.classList.add("link");
-          link.href = image.link;
-          link.target = "_blank";
-          textWrapper.appendChild(link);
-        }
-        //style background of image wrapper
-        imageWrapper.style.backgroundImage = `url(${
-          image.image ? image.image : "none"
-        })`;
-        imageWrapper.appendChild(textWrapper);
-        profileSection.appendChild(imageWrapper);
+        return new ImageProfile(image);
       });
     });
 };
 /**
  * assign footer links for mobile
  */
-createFooterLinks = () => {
+const createFooterLinks = () => {
   const list = document.createElement("ul");
   list.classList.add("footer-icon-list");
   footer.appendChild(list);
@@ -158,23 +280,16 @@ createFooterLinks = () => {
     .then((data) => {
       social = data;
       social?.socialLinks.map((social) => {
-        const footerIcon = document.createElement("img");
-        footerIcon.src = `./img/${social.type}.svg`;
-        const footerLink = document.createElement("a");
-        footerLink.href = social.link;
-        const linkText = document.createTextNode(`${social.type}`);
-        footerLink.appendChild(linkText);
-        const listItem = document.createElement("li");
-        listItem.appendChild(footerIcon);
-        listItem.appendChild(footerLink);
-        list.appendChild(listItem);
+        if (!social) return;
+        return new FooterIcon(list, social);
       });
+      return new FooterNavigationArrow();
     });
 };
 /**
  * create all elements for the page
  */
-createElements = () => {
+const createElements = () => {
   nav = document.createElement("nav");
   heroSection = document.createElement("header");
   profileSection = document.createElement("section");
@@ -183,46 +298,35 @@ createElements = () => {
 /**
  * assign classes to newly created elements
  */
-styleElements = () => {
+const styleElements = () => {
   nav.classList.add("nav");
   heroSection.classList.add("hero");
   profileSection.classList.add("profile");
   footer.classList.add("footer");
 };
 /**
- * render the up arrow in the footer
+ * assign new elements to document
  */
-createFooterUpArrow = () => {
-  const arrow = document.createElement("img");
-  arrow.src = "./img/up-arrow.svg";
-  arrow.classList.add("up-arrow");
-  arrow.onclick = () => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-  };
-  footer.appendChild(arrow);
-};
-/**
- * build out the template for the browser
- */
-createTemplate = () => {
+const prepareDocument = () => {
   const boiler = document.getElementById("boiler-plate");
-  createElements();
-  styleElements();
-  assignSocialMedia();
-  assignHero();
-  assignImages();
-  createFooterLinks();
-  createFooterUpArrow();
   document.body.insertBefore(heroSection, boiler);
   document.body.insertBefore(nav, heroSection);
   document.body.insertBefore(profileSection, boiler);
   document.body.insertBefore(footer, boiler);
 };
 /**
- * on document load, create a the template
+ * build out the template for the browser
  */
-document.body.onload = createTemplate();
+const createTemplate = () => {
+  createElements();
+  styleElements();
+  assignSocialMediaIcons();
+  assignHero();
+  assignImages();
+  createFooterLinks();
+  prepareDocument();
+};
+
+if (document.body) {
+  document.body.onload = () => createTemplate();
+}
