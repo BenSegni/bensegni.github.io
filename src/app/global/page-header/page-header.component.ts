ad92d6262ecbd7737f0820f29ae8c633de96b567
Ben Segni <ben.segni@ba.com>
2024-04-11 14:55:19 +0100

    new page header design

--- src/app/global/page-header/page-header.component.ts
+++ src/app/global/page-header/page-header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 interface Header {
-    header: string;
-    title: string;
+    greeting: string;
+    jobTitle: string;
+    skills: string[];
     location: string;
     locationLink: string;
     logoPath: string;
@@ -14,8 +15,9 @@ interface Header {
 })
 export class PageHeaderComponent {
     public headerContent: Header = {
-        header: 'Ben Segni',
-        title: 'Lead Full Stack Software Engineer &amp; UI Designer.',
+        greeting: "Hi, I'm <span>Ben</span>.",
+        jobTitle: 'A Frontend <span>full-stack developer</span>.',
+        skills: ['Angular', 'HTML5', 'Jasmine', 'NestJs', 'NgRx', 'ReactJs', 'RxJs', 'SASS', 'Typescript'],
         location: 'County Durham, United Kingdom.',
         locationLink: 'https://maps.app.goo.gl/X1s9Tfpd2ZJWqGSN7',
         logoPath: '../../../assets/img/me/me_3.webp'
